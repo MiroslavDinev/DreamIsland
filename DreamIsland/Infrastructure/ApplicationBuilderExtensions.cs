d25d27fc5e385812c95f77e1c93b2083a65dc042
Miroslav Dinev <miroslavdinev@yahoo.com>
2021-12-19 11:55:11 +0200

    Added Admin role

--- DreamIsland/Infrastructure/ApplicationBuilderExtensions.cs
+++ DreamIsland/Infrastructure/ApplicationBuilderExtensions.cs
@@ -1,22 +1,65 @@
 ﻿namespace DreamIsland.Infrastructure
 {
+    using System;
+    using System.Threading.Tasks;
+
     using Microsoft.AspNetCore.Builder;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.Extensions.DependencyInjection;
 
     using DreamIsland.Data;
+    using DreamIsland.Data.Models;
+
+    using static WebConstants;
 
     public static class ApplicationBuilderExtensions
     {
         public static IApplicationBuilder PrepareDatabase(this IApplicationBuilder app)
         {
-            using var scoperServices = app.ApplicationServices.CreateScope();
+            using var scopedServices = app.ApplicationServices.CreateScope();
+            var serviceProvider = scopedServices.ServiceProvider;
 
-            var data = scoperServices.ServiceProvider.GetService<DreamIslandDbContext>();
+            var data = serviceProvider.GetRequiredService<DreamIslandDbContext>();
 
             data.Database.Migrate();
 
+            SeedAdministrator(serviceProvider);
+
             return app;
+        }
+
+        private static void SeedAdministrator(IServiceProvider services)
+        {
+            var userManager = services.GetRequiredService<UserManager<User>>();
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+            Task.Run(async () =>
+               {
+                   if(await roleManager.RoleExistsAsync(AdministratorRoleName))
+                   {
+                       return;
+                   }
+
+                   var role = new IdentityRole { Name = AdministratorRoleName };
+                   await roleManager.CreateAsync(role);
+
+                   const string adminEmail = "admin.dreamisland@dir.bg";
+                   const string adminPassword = "admin123!321!";
+
+                   var user = new User
+                   {
+                       Email = adminEmail,
+                       UserName = adminEmail,
+                       Nickname = "Admin"
+                   };
+
+                   await userManager.CreateAsync(user, adminPassword);
+                   await userManager.AddToRoleAsync(user, role.Name);
+
+               })
+                .GetAwaiter()
+                .GetResult();
         }
     }
 }
