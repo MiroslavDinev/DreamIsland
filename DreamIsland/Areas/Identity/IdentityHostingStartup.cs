e9594ef16a82865c58f2a1bd13e15f789bc1c030
Miroslav Dinev <miroslavdinev@yahoo.com>
2022-02-01 19:18:15 +0200

    Cleared namespaces

--- DreamIsland/Areas/Identity/IdentityHostingStartup.cs
+++ DreamIsland/Areas/Identity/IdentityHostingStartup.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using DreamIsland.Data;
-using DreamIsland.Data.Models;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Hosting;
 
 [assembly: HostingStartup(typeof(DreamIsland.Areas.Identity.IdentityHostingStartup))]
 namespace DreamIsland.Areas.Identity
