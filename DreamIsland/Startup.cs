853a1e7b90781ffde0245bd9c301df76c3fac4cb
Miroslav Dinev <miroslavdinev@yahoo.com>
2022-03-29 21:34:21 +0300

    Cleanup of unnecessary code

--- DreamIsland/Startup.cs
+++ DreamIsland/Startup.cs
@@ -67,7 +67,7 @@ namespace DreamIsland
                 // This lambda determines whether user consent for non-essential 
                 // cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
-                // requires using Microsoft.AspNetCore.Http;
+
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
